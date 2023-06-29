5844fd186363c440703c8476f36cd06d08413321
Memphis DiAngelis <earthtomemphis@gmail.com>
2023-06-29 16:21:06 -0600

    variables

---
+++ 01-JavascriptSandbox/01-Variables-Data-Types/02-variables/script.js
@@ -0,0 +1,62 @@
+// Ways to declare a variable
+// var, let, const
+
+let firstName = 'John';
+let lastName = 'Doe';
+console.log(firstName, lastName);
+
+let age = 30;
+console.log(age);
+
+// Naming Conventions
+// - Only letters, numbers, underscores, dollar signs
+// Can't start with a number
+
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+// Re-assigning Variables
+age = 31;
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if (true) {
+	score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+const arr = [1, 2, 3, 4];
+
+arr.push(5);
+
+console.log(arr);
+
+const person = {
+	name: 'Brad',
+};
+
+person.name = 'John';
+
+person.email = 'john@gmail.com';
+
+console.log(person);
+
+// Declaring multiple values at once
+let a, b, c;
+
+const d = 10,
+	e = 20,
+	f = 30;
+
+console.log(d, e, f);
